b8ab9dfb548cbe17b84104182486991832349296
Valentin <bagadin@inbox.ru>
2021-04-14 17:16:38 +0500

    New init: project updated

--- src/index.js
+++ src/index.js
@@ -1,17 +1,31 @@
 // @ts-check
 
-import 'core-js/stable';
-import 'regenerator-runtime/runtime';
+import 'core-js/stable/index.js';
+import 'regenerator-runtime/runtime.js';
 
 import '../assets/application.scss';
-
-import gon from 'gon';
-import app from './app';
 
 if (process.env.NODE_ENV !== 'production') {
   localStorage.debug = 'chat:*';
 }
 
-app(gon);
+const p = document.createElement('p');
+p.classList.add('card-text');
+p.textContent = 'It works!';
 
-console.log('gon', gon);
+const h5 = document.createElement('h5');
+h5.classList.add('card-title');
+h5.textContent = 'Project frontend l4 boilerplate';
+
+const cardBody = document.createElement('div');
+cardBody.classList.add('card-body');
+cardBody.append(h5, p);
+
+const card = document.createElement('div');
+card.classList.add('card', 'text-center');
+card.append(cardBody);
+
+const container = document.querySelector('#chat');
+container.append(card);
+
+console.log('it works!');
