6fd10517887670ab4649448782c8ad98616ac7bc
Valentin <bagadin@inbox.ru>
2021-04-23 15:41:57 +0500

    Переименовывает authContext

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -6,7 +6,7 @@ import {
   Redirect,
 } from 'react-router-dom';
 
-import authContext from '../contexts/index.jsx';
+import authContext from '../contexts/authContext.js';
 import useAuth from '../hooks/index.jsx';
 
 import Nav from './Nav.jsx';
