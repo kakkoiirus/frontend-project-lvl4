f304250fec4a0db1ad145274df5ec89d3717eccb
Valentin <bagadin@inbox.ru>
2021-04-24 12:13:49 +0500

    Переносит логику внуть экспортируемой функции

--- src/init.jsx
+++ src/init.jsx
@@ -17,67 +17,67 @@ import { addMessage } from './slices/messages.js';
 
 import App from './components/App.jsx';
 
-if (process.env.NODE_ENV === 'production') {
-  const rollbar = new Rollbar();
-  rollbar.configure({
-    accessToken: '273512464d8d489e8d131086a5634ce7',
-    captureUncaught: true,
-    captureUnhandledRejections: true,
-    payload: {
-      environment: 'production',
-    },
-  });
-}
-
-i18n
-  .use(initReactI18next)
-  .init({
-    resources,
-    lng: 'ru',
-
-    interpolation: {
-      escapeValue: false,
-    },
-  });
-
-const store = configureStore({
-  reducer: rootReducer,
-});
-
-const socket = io();
-
-socket.on('newChannel', (channel) => {
-  store.dispatch(addChannel(channel));
-});
-
-socket.on('removeChannel', ({ id }) => {
-  store.dispatch(removeChannel({ id }));
-});
-
-socket.on('renameChannel', (channel) => {
-  store.dispatch(renameChannel(channel));
-});
-
-socket.on('newMessage', (message) => {
-  store.dispatch(addMessage(message));
-});
-
-const serverContextValue = {
-  createChannel: (name, acknowledge) => {
-    socket.emit('newChannel', { name }, acknowledge);
-  },
-  removeChannel: (id, acknowledge) => {
-    socket.emit('removeChannel', { id }, acknowledge);
-  },
-  renameChannel: ({ id, name }, acknowledge) => {
-    socket.emit('renameChannel', { id, name }, acknowledge);
-  },
-  sendMessage: (message, acknowledge) => {
-    socket.emit('newMessage', message, acknowledge);
-  },
-};
-
 export default () => {
+  if (process.env.NODE_ENV === 'production') {
+    const rollbar = new Rollbar();
+    rollbar.configure({
+      accessToken: '273512464d8d489e8d131086a5634ce7',
+      captureUncaught: true,
+      captureUnhandledRejections: true,
+      payload: {
+        environment: 'production',
+      },
+    });
+  }
+
+  i18n
+    .use(initReactI18next)
+    .init({
+      resources,
+      lng: 'ru',
+
+      interpolation: {
+        escapeValue: false,
+      },
+    });
+
+  const store = configureStore({
+    reducer: rootReducer,
+  });
+
+  const socket = io();
+
+  socket.on('newChannel', (channel) => {
+    store.dispatch(addChannel(channel));
+  });
+
+  socket.on('removeChannel', ({ id }) => {
+    store.dispatch(removeChannel({ id }));
+  });
+
+  socket.on('renameChannel', (channel) => {
+    store.dispatch(renameChannel(channel));
+  });
+
+  socket.on('newMessage', (message) => {
+    store.dispatch(addMessage(message));
+  });
+
+  const serverContextValue = {
+    createChannel: (name, acknowledge) => {
+      socket.emit('newChannel', { name }, acknowledge);
+    },
+    removeChannel: (id, acknowledge) => {
+      socket.emit('removeChannel', { id }, acknowledge);
+    },
+    renameChannel: ({ id, name }, acknowledge) => {
+      socket.emit('renameChannel', { id, name }, acknowledge);
+    },
+    sendMessage: (message, acknowledge) => {
+      socket.emit('newMessage', message, acknowledge);
+    },
+  };
+
   ReactDOM.render(
     <I18nextProvider i18n={i18n}>
       <Provider store={store}>
