48de3f6cf82f7245157d0576aa92eb02d0205ee0
Valentin <bagadin@inbox.ru>
2021-04-27 13:53:09 +0500

    Revert "Refactoring"
    This reverts commit 6376af6a788dd6c5094dc7c74125a9c3253d9c85.

--- src/init.jsx
+++ src/init.jsx
@@ -15,7 +15,7 @@ import { addMessage } from './slices/messages.js';
 
 import App from './components/App.jsx';
 
-export default async ({ socketClient: socket }) => {
+export default async (socket) => {
   if (process.env.NODE_ENV === 'production') {
     const rollbar = new Rollbar();
     rollbar.configure({
