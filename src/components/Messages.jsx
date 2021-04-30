178d29afb252afab745640af013d972c9f49d6c3
Valentin <bagadin@inbox.ru>
2021-04-30 11:50:02 +0500

    Добавляет скролл к последнему сообщению

--- src/components/Messages.jsx
+++ src/components/Messages.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useRef, useEffect } from 'react';
 import { useSelector } from 'react-redux';
 
 import ChatBox from './ChatBox.jsx';
@@ -7,15 +7,26 @@ const Messages = () => {
   const { messages } = useSelector((state) => state.messagesData);
   const channel = useSelector((state) => state.channelsData);
 
+  const messagesRef = useRef();
+
+  useEffect(() => {
+    if (messagesRef.current) {
+      messagesRef.current.scrollIntoView(false);
+    }
+  }, [messages]);
+
   return (
     <>
-      <div className="chat-messages overflow-auto mb-3">
+      <div
+        className="chat-messages overflow-auto mb-3"
+      >
         {messages
           .filter((m) => m.channelId === channel.currentChannelId)
-          .map((m) => (
+          .map((m, index, arr) => (
             <div
               key={m.id}
               className="text-break"
+              ref={arr.length - 1 === index ? messagesRef : null}
             >
               <b>
                 {m.username}
